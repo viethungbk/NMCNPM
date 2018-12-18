164814db46bd4dc81db813c95c0349aecbc9f38f
viethungbk <anhhung021098@gmail.com>
2018-12-19 02:11:13 +0700

    Hung update footer

--- PHONE/application/views/site/footer.php
+++ PHONE/application/views/site/footer.php
@@ -7,13 +7,9 @@
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
 
-<div id="footer_text"> <!-- The footer_text --> 
-	<strong><h3>Bài tập lớn Nhập môn công nghệ phần mềm. - Nhóm 10</h3></strong>
-	<br>
-	<p><h4>Giảng viên hướng dẫn: <i>TS. Bùi Thị Mai Anh</i> </h5></p>
-	
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
-</div><!-- End footer_text -->
+
 
 <div id="footer_face">  <!-- The footer_face -->            
 	<a title="trên facebook" target="_blank" href="https://www.facebook.com/groups/374949286661756/" rel="nofollow">
@@ -30,65 +26,6 @@
 </div><!-- End footer_face -->
 <div class="clear"></div><!-- clear float --> 		        <!-- End box-footer -->
 
-<!-- Contact area starts -->
-<!--
-    <section id="contact" class="contact-area section-big">
-
-    	<div class="contact">
-	        <div class="container">
-
-	            <div class="row">
-	                <div class="col-md-12 text-center">
-	                    <div class="section-title">
-	                        <h2>Contact Us</h2>
-	                    </div>
-	                </div>
-	            </div>
-
-				<div class="row">
-					<div class="col-md-3">
-						<div class="address">
-		                    <div class="">
-		                        <i class="fa fa-home"></i>
-		                        <p>Số 1 Đại Cồ Việt, Hai Bà Trưng, Hà Nội</p>
-		                    </div>
-		                    <div class="address-box clearfix">
-		                    	<i class="fas fa-mobile-alt"></i>
-		                        <p><a href="tel:033 999 999">033 999 999 (Mr. Hùng)</a>
-		                    </div>
-		                    <div class="address-box clearfix">
-		                        <i class="fa fa-envelope"></i>
-		                        <p><a href="mailto:hung.nv161974@sis.hust.edu.vn">hung.nv161974@sis.hust.edu.vn</a>
-		                    </div>
-		                    <div class="address-box clearfix">
-		                        <i class="fas fa-globe"></i>
-		                        <p><a href="http://>www.bk.edu.vn">www.bk.edu.vn</a></p>
-		                    </div>
-		                    <div class="address-box clearfix">
-		                        <i class="fab fa-facebook-f"></i>
-		                        <p><a href="https://www.facebook.com/">Like our fan page</a></p>
-		                    </div>
-	                	</div>
-					</div>
-
-					<div class="col-md-9">
-						  <div class="contact-address-box clearfix">
-
-	               
-
-	                <div class="contact-form clearfix">
-	                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7449.419253995984!2d105.84151262483981!3d21.00427374404641!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac768ffe1abd%3A0x22b136bcf1c08e2a!2zQsOhY2ggS2hvYSwgSGFpIELDoCBUcsawbmcsIEjDoCBO4buZaQ!5e0!3m2!1svi!2s!4v1545144516522" width="1000" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
-	                </div>
-	            </div> 
-					</div>
-				</div>
-				
-	          
-
-	        </div>
-        </div>
-    </section>
-    Contact area ends -->
 
 <!-- Contact area starts -->
     <section id="contact" class="contact-area section-big">
@@ -115,7 +52,7 @@
                     </div>
                     <div class="address-box clearfix">
                         <i class="fa fa-envelope"></i>
-		                <p><a href="mailto:hung.nv161974@sis.hust.edu.vn">hung.nv161974@sis.hust.edu.vn</a>
+		                <p><a href="mailto:hung.nv161974@sis.hust.edu.vn">hung.nv@hust.edu.vn</a>
                     </div>
                     <div class="address-box clearfix">
                         <i class="fas fa-globe"></i>
@@ -135,3 +72,11 @@
         </div>
     </section>
     <!-- Contact area ends -->
+
+<div id="footer_text"> <!-- The footer_text --> 
+	<strong><h3>Bài tập lớn Nhập môn công nghệ phần mềm. - Nhóm 10</h3></strong>
+	<br>
+	<p><h4>Giảng viên hướng dẫn: <i>TS. Bùi Thị Mai Anh</i> </h5></p>
+	
+
+</div><!-- End footer_text -->
